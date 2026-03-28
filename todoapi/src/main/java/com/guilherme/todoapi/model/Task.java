6bf2686eca31beaecf148ebc1f2ac2309395fcf3
Guilherme Jacob Berlez <guilhermeberlez@gmail.com>
2026-03-27 23:10:30 -0300

    Adição de validações na classe Task

--- todoapi/src/main/java/com/guilherme/todoapi/model/Task.java
+++ todoapi/src/main/java/com/guilherme/todoapi/model/Task.java
@@ -4,7 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import org.springframework.data.jpa.repository.EntityGraph;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Task {
@@ -13,7 +14,13 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY) //Realiza o AUTO_INCREMENT no banco, adicionando mais um no valor do ID na tabela
     // A Primary Key é sempre o atributo a que está abaixo dessas linhas acima!!
     private Long id;
+
+    @NotBlank(message = "O título precisa ser prenchido!")
+    @Size(min = 3, message = "O título deve ter pelo menos 3 caracteres")
     private String title;
+
+    @NotBlank(message = "A descrição precisa ser preenchida!")
+    @Size(min = 5, message = "A descrição deve ter pelo menos 5 caracteres")
     private String description;
     private boolean completed;
 
