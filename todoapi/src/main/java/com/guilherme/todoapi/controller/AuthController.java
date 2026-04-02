0a65271c84147b91e84d9cfac91441b0eaca774d
Guilherme Jacob Berlez <guilhermeberlez@gmail.com>
2026-04-01 23:12:50 -0300

    Adição da autenticação de login

---
+++ todoapi/src/main/java/com/guilherme/todoapi/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.guilherme.todoapi.controller;
+
+import com.guilherme.todoapi.model.User;
+import com.guilherme.todoapi.repository.UserRepository;
+import com.guilherme.todoapi.security.JwtUtil;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserRepository userRepository;
+
+    public AuthController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+
+        User dbUser = userRepository.findByUsername(user.getUsername())
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+        if (!dbUser.getPassword().equals(user.getPassword())) {
+            throw new RuntimeException("Senha inválida");
+        }
+
+        return JwtUtil.generateToken(user.getUsername());
+
+
+    }
+}
---
+++ todoapi/src/main/java/com/guilherme/todoapi/security/JwtUtil.java
@@ -0,0 +1,31 @@
+package com.guilherme.todoapi.security;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+
+// Lógica de segurança (token)
+public class JwtUtil {
+
+        private static final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+        public static String generateToken(String username) {
+            return Jwts.builder()
+                    .setSubject(username)
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                    .signWith(key)
+                    .compact();
+        }
+
+        public static String extractUsername(String token) {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject();
+        }
+
+}
