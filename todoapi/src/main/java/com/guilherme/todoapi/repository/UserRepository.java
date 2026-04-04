59c59257374a0847ba5c7e6a4f481d0065a8a467
Guilherme Jacob Berlez <guilhermeberlez@gmail.com>
2026-04-04 15:41:16 -0300

    commit

--- todoapi/src/main/java/com/guilherme/todoapi/repository/UserRepository.java
+++ todoapi/src/main/java/com/guilherme/todoapi/repository/UserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // Optional é um "envelope" que pode ou não conter um valor. É a forma do Java dizer: "pode ser que esse usuário não exista"
     Optional<User> findByUsername(String username);// Precisamos buscar o usuário no login
 }
