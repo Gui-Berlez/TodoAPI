a03b565d0597f3512de44764ed52eaf2b745731e
Guilherme Jacob Berlez <guilhermeberlez@gmail.com>
2026-04-10 21:55:33 -0300

    Ajustes de injeção no TaskController

--- todoapi/src/main/java/com/guilherme/todoapi/controller/TaskController.java
+++ todoapi/src/main/java/com/guilherme/todoapi/controller/TaskController.java
@@ -10,40 +10,45 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
 
     // Com o @Autowired é como se o Spring falasse: "vou pegar o Bean TaskService e colocar aqui". Por isso que chamamos as classes de TaskService mesmo sem instâncias.
-    @Autowired //Esse comando me dá uma instancia do atributo abaixo, no caso da classe TaskService;
-    private TaskService taskService;
+    // @Autowired - Esse comando me dá uma instancia do atributo abaixo, no caso da classe TaskService;
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
 // POST
-    @PostMapping("/tasks")
+    @PostMapping
     public Task createTask(@Valid @RequestBody Task task){
         // Spring verifica as anotações (@NotBlank, @Size...) ANTES de entrar no metodo
         return taskService.createTask(task);
     }
 //------------------------------------------------------------------------------------------------------------
 // GET
-    @GetMapping("/tasks")
+    @GetMapping
     public List<Task> getTasks(){
 
         return taskService.getTasks();
     }
 
-    @GetMapping("/tasks/{id}")
+    @GetMapping("/{id}")
     public Task getTaskById(@PathVariable Long id){
         return taskService.getTaskById(id);
     }
 
 //------------------------------------------------------------------------------------------------------------
 // DELETE
-    @DeleteMapping("/tasks/{id}")
+    @DeleteMapping("/{id}")
     public void deleteTask(@PathVariable Long id){
         taskService.deleteTask(id);
     }
 //------------------------------------------------------------------------------------------------------------
 // PUT
-    @PutMapping("/tasks/{id}")
+    @PutMapping("/{id}")
     public Task updateTask(@PathVariable Long id, @Valid @RequestBody Task task){
         return taskService.updateTask(id, task);
     }
